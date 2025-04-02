bbe6bc32609b3ad54b3ffe469bb1514f25f4a570
Jan Korona <jan.korona@gmail.com>
2025-04-02 14:07:57 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-import { HStack, Button } from "@chakra-ui/react";
+import { Center, Heading, Text } from "@chakra-ui/react";
 function App() {
   return (
-    <HStack gap="3">
-      <Button>Better Workouts</Button>
-      <Button>Under construction</Button>
-    </HStack>
+    <Center gap="3">
+      <Heading size="3xl">Better Workouts</Heading>
+      <Text>Under construction</Text>
+    </Center>
   );
 }
 
