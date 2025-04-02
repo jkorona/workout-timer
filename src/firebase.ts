d00eba836eac4e5cb8026d6d03bafffaa29762fe
Jan Korona <jan.korona@gmail.com>
2025-04-02 13:28:44 +0200

    new delpoyment

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,9 +8,9 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "workout-timer-platform.firebaseapp.com",
-  projectId: "workout-timer-platform",
-  storageBucket: "workout-timer-platform.firebasestorage.app",
+  authDomain: "better-workouts.firebaseapp.com",
+  projectId: "better-workouts",
+  storageBucket: "better-workouts.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
