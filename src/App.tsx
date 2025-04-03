e5686031f5582ffa66e8d4b06dbaca548cc7edaa
Jan Korona <jan.korona@gmail.com>
2025-04-03 11:34:44 +0200

    center

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Center, Heading, Stack, Text } from "@chakra-ui/react";
 function App() {
   return (
     <Center h="100vh">
-      <Stack gap="4" justifyContent="center">
+      <Stack gap="4" alignItems="center">
         <Heading size="4xl">Better Workouts</Heading>
         <Text>Under construction</Text>
       </Stack>
